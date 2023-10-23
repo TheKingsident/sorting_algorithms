294d0081030378ef7c68817bb48290d9e9b03e68
TheKingsident <kingsident@gmail.com>
2023-10-23 13:12:25 +0100

    Added new files

--- sort.h
+++ sort.h
@@ -25,5 +25,6 @@ void print_array(const int *array, size_t size);
 void bubble_sort(int *array, size_t size);
 void insertion_sort_list(listint_t **list);
 size_t get_length(listint_t **list);
+listint_t *create_listint(const int *array, size_t size);
 
 #endif
