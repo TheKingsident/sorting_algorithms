8add567471306f7e35f07eca8983284d4d8f62bf
TheKingsident <kingsident@gmail.com>
2023-10-23 21:02:31 +0100

    Added new files

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -12,6 +12,12 @@ void selection_sort(int *array, size_t size)
 {
 	size_t i, j, min_idx;
 
+	if (array == NULL || size < 2)
+	{
+		return;
+	}
+
+
 	for (i = 0; i < size - 1; i++)
 	{
 		min_idx = i;
